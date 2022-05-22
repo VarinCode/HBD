29d703ba7fef5ce5bf3e43f7a6adab74ae21fc2f
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-05-22 11:52:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// run terminal: node script.js
+class birthday {
+
+    constructor(countdown_start) {
+        this.countdown = countdown_start;
+        this.one = 1
+        this.HBD = ["\nHappy Birthday to You 🎉✨🎁",
+            "Today I wish you a fun time, shared with your dear ones, and a lifelong happiness!\n",
+            // .....
+        ];
+    };
+
+    start = () => {
+        while (true) {
+            if (this.countdown >= this.one) {
+                this.countdown--
+                    console.log("เริ่มนับถอยหลังใน " + this.countdown);
+                this.complete(); // callback เมื่อเงื่อนไขเป็นจริง
+            };
+            break;
+        };
+    };
+
+    complete = function() {
+        if (this.countdown == 1) {
+            setTimeout(() => this.HBD.forEach((msg) => {
+                console.log(msg.toString())
+            }), 3000);
+            return clearInterval(delay);
+        };
+    };
+};
+
+const happy = new birthday(6); // ตั้งค่านับถอยหลัง
+var delay = setInterval(happy.start, 1000); // ตั้งค่าหน่วงเวลาหน่วยเป็น วินาที
